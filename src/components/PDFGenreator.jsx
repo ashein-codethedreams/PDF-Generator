d039fd66f34aca5c38ea19f27fab06234f643f71
unknown <heroglasses155@gmail.com>
2023-12-06 15:56:45 +0630

    generate with button and insert pages and image

--- src/components/PDFGenreator.jsx
+++ src/components/PDFGenreator.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   Document,
   Page,
@@ -5,41 +6,89 @@ import {
   View,
   StyleSheet,
   PDFViewer,
+  Image,
 } from "@react-pdf/renderer";
+import { Button } from "antd";
+
 // Create styles
 const styles = StyleSheet.create({
-  page: {
-    backgroundColor: "black",
-    color: "white",
-  },
   section: {
     margin: 10,
     padding: 10,
   },
+  viewerContainer: {
+    overflow: "hidden",
+    height: "100vh", // Set the height to the full viewport height
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   viewer: {
-    width: window.innerWidth, //the pdf viewer will take up all of the width and height
-    height: window.innerHeight,
+    width: "100%", // Take up full width
+    height: "100%", // Take up full height
+  },
+  imageContainer: {
+    height: "100vh", // Set the height to the full viewport height
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 500,
+    height: 600,
   },
 });
 
 // Create Document Component
 function PDFGenerator() {
+  const [showPDF, setShowPDF] = useState(false);
+
+  const generatePDF = () => {
+    setShowPDF(true);
+  };
+
   return (
-    <PDFViewer style={styles.viewer}>
-      <Document>
-        <Page size="A4" style={styles.page}>
-          <View style={styles.section}>
-            <Text>Hello</Text>
-          </View>
-          <View style={styles.section}>
-            <Text>World</Text>
-          </View>
-          <View style={styles.section}>
-            <Text>I am text to PDF Generator </Text>
-          </View>
-        </Page>
-      </Document>
-    </PDFViewer>
+    <div style={styles.viewerContainer}>
+      {!showPDF ? (
+        <Button
+          onClick={generatePDF}
+          style={{
+            backgroundColor: "rgb(82, 86, 89)",
+            color: "white",
+          }}
+        >
+          Generate PDF
+        </Button>
+      ) : (
+        <PDFViewer style={styles.viewer}>
+          <Document>
+            <Page size="A4">
+              <View style={styles.section}>
+                <Text style={{ textAlign: "center" }}>PDF Testing</Text>
+              </View>
+              <View style={styles.section}>
+                <Text>Hello</Text>
+              </View>
+              <View style={styles.section}>
+                <Text>World</Text>
+              </View>
+              <View style={styles.section}>
+                <Text>I am text to PDF Generator </Text>
+              </View>
+            </Page>
+            <Page size="A4" style={styles.imageContainer}>
+              <View style={styles.section}>
+                <Text style={{ textAlign: "center" }}>Second Page</Text>
+              </View>
+              <Image src="./us.jpg" style={styles.image} alt="img" />
+            </Page>
+          </Document>
+        </PDFViewer>
+      )}
+    </div>
   );
 }
+
 export default PDFGenerator;
