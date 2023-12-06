1a2f8183efffe621bf62aa55b01b76361ba3975e
unknown <heroglasses155@gmail.com>
2023-12-06 14:37:48 +0630

    @react-pdf/renderer testing

---
+++ src/components/PDFGenreator.jsx
@@ -0,0 +1,45 @@
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFViewer,
+} from "@react-pdf/renderer";
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "black",
+    color: "white",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+  },
+  viewer: {
+    width: window.innerWidth, //the pdf viewer will take up all of the width and height
+    height: window.innerHeight,
+  },
+});
+
+// Create Document Component
+function PDFGenerator() {
+  return (
+    <PDFViewer style={styles.viewer}>
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <Text>Hello</Text>
+          </View>
+          <View style={styles.section}>
+            <Text>World</Text>
+          </View>
+          <View style={styles.section}>
+            <Text>I am text to PDF Generator </Text>
+          </View>
+        </Page>
+      </Document>
+    </PDFViewer>
+  );
+}
+export default PDFGenerator;
